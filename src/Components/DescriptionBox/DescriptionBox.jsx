3f8c5ea2380856e1f22b0f6cd177f10c384e826c
KARTIK5799 <thekartiksapkal@gmail.com>
2024-02-08 17:50:52 +0530

    third

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="description-nevigator">
+        <div className="description-nav-box">Description</div>
+        <div className="description-nav-box fade"> Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+          placeat, voluptatem velit blanditiis officia laboriosam molestiae
+          mollitia vel esse architecto quam fugit doloribus quia iusto illo nisi
+          accusantium beatae animi ipsa qui similique ea. Autem inventore minima
+          cum dicta reprehenderit aspernatur temporibus, quae corrupti. Laborum
+          modi quia quos ullam tempora earum unde deserunt deleniti est hic fuga
+          soluta sapiente eligendi sit, ipsum in possimus repellat. Porro libero
+          rem ipsa deserunt veniam quas esse quasi nemo odit voluptas et itaque
+          enim voluptates commodi optio ratione perspiciatis corrupti, ut
+          architecto officia consequuntur nobis sint maxime hic? Esse saepe qui
+          ratione maxime odit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+          totam necessitatibus deserunt obcaecati, ipsum recusandae enim sint
+          consequuntur error libero molestiae eum consequatur distinctio dolor
+          iste ab saepe laborum harum. 
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import ProductDisaplay from '../Components/ProductDisaplay/ProductDisaplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 
 const Product = () => {
   const {all_product}=useContext(ShopContext);
@@ -10,6 +12,8 @@ const Product = () => {
   return (
     <div>
         <Breadcrum product={product}/>
+        <ProductDisaplay product={product}/>
+        <DescriptionBox/>
     </div>
   )
 }
